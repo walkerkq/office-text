72f37df350d4c9851cc031b18255639e884b1250
Kaylin Walker <walkerkq2@gmail.com>
2018-02-01 10:15:07 -0600

    Begin knn

--- office-tidy.R
+++ office-tidy.R
@@ -1,15 +1,14 @@
-#' ---
-#' output: github_document
-#' ---
 library(dplyr)
 library(tidytext)
 library(tidyr)
 library(corrplot) 
+library(class)
 
 office <- read.csv("office-lines.csv", stringsAsFactors=F) 
 
-# top 20 speakers
-main_characters <- data.frame(table(office$speaker)) %>% top_n(20, Freq)
+# top speakers
+top20 <- data.frame(table(office$speaker)) %>% top_n(20, Freq)
+top5 <- data.frame(table(office$speaker)) %>% top_n(5, Freq)
 
 # tidy the text
 office_tidy <- office %>%
@@ -31,11 +30,11 @@ office_tf <- office_tidy %>%
   ungroup() %>%
   subset(tf_idf > 0 & word.total > 10 
          & word.pct < 1 & n > 5 
-         & speaker %in% main_characters$Var1)
+         & speaker %in% top20$Var1)
 
 # stage for finding correlation between speakers
 office_corr <- office_tidy %>%
-  subset(speaker %in% main_characters$Var1) %>%
+  subset(speaker %in% top20$Var1) %>%
   group_by(speaker) %>%
   select(-n) %>%
   spread(speaker, Prop) 
@@ -62,4 +61,41 @@ plot(fit,main="Cluster Dendrogram of Speakers", family="Avenir")
 rect.hclust(fit, k=3, border="darkseagreen") 
 
 # use knn to classify new lines of text
+office_class <- office %>%
+  mutate(speaker = ifelse(speaker %in% top5$Var1, speaker, "Other"),
+         id2 = paste(season, episode, scene, sep=".")) %>%
+  subset(!speaker %in% "Other") %>%
+  unnest_tokens(word, line_text) %>%
+  anti_join(stop_words) %>%
+  count(word, speaker, id2) %>%
+  group_by(id2) %>%
+  mutate(Prop = n / sum(n))  %>%
+  subset(Prop >= .005) %>%
+  select(-n) %>%
+  spread(word, Prop)
+office_class[is.na(office_class)] <- 0
 
+set.seed(337)
+subset_ids <- sample(1:length(office_class$speaker), length(office_class$speaker)*.50, replace=F)  
+office_class_subset <- office_class[subset_ids, ]
+
+train_ids <- sample(length(office_class_subset$speaker), length(office_class_subset$speaker)*.80, replace=F)
+train_bk <- office_class_subset[train_ids, -c(1,2)] # training
+test_bk <- office_class_subset[-train_ids, -c(1,2)] # testing
+train_resp <- office_class_subset[train_ids, c(1,2)] # groups for training
+test_resp <- office_class_subset[-train_ids, c(1,2)] # groups for testing
+
+# run
+system.time(kresult <- knn(train = train_bk, test = test_bk, cl = train_resp$speaker, k = 1, use.all=F))
+#30 min... wowza
+
+# check results
+knn_results <- data.frame(round(prop.table(table(kresult, test_resp$speaker),2),2)) %>%
+  set_colnames(c("Classification", "Actual", "Pct"))
+
+ggplot(knn_results, aes(Actual, Pct, fill=Classification)) + 
+  geom_bar(stat="identity", position="dodge", alpha=0.75) +
+  labs(title="Accuracy of KNN Classification into HClusters\n", y = "Percent") + 
+  theme_classic(base_size=10, base_family="Avenir") + 
+  theme(axis.text.x=element_text(angle=30, hjust=1)) + ylim(c(0,1))
+# very poor
